15ce969ff8db7a4797fa56441c7028bd78cd1e2f
Jan Paul Ettles <JanPaulEttles>
2017-07-12 16:33:14 +0100

    general update

--- janapp/app.js
+++ janapp/app.js
@@ -1,6 +1,18 @@
+/*
+npm install express
+npm install body-parser
+npm install cookie-parser
+npm install tinycache
+npm install node-uuid
+*/
+
 var express = require('express');
 var bodyParser = require('body-parser')
 var cookieParser = require('cookie-parser');
+var tinycache = require('tinycache');
+var uuid = require('node-uuid');
+
+var db = new tinycache();
 
 var server = express();
 server.set('port', 3000);
@@ -10,37 +22,65 @@ server.use(cookieParser());
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({ extended: true }));
 
-server.get('/reset', function(req, res) {
-	//console.log('you posted:  ' + JSON.stringify(req.body));
-	//console.log('Cookies: ', req.cookies);
-
-	res.setHeader('Content-Type', 'text/html; charset=utf-8');
-	res.setHeader('X-XSS-Protection', '0'); //ERR_BLOCKED_BY_XSS_AUDITOR
-
-        req.session = null;
-        res.clearCookie('Session-Token', { path: '/' });
-
-        res.redirect('http://localhost:3000/index.html');
-});
 
 server.get('/authenticated', function(req, res) {
-	//console.log('you posted:  ' + JSON.stringify(req.body));
-	//console.log('Cookies: ', req.cookies);
+
+	if(!isAuthenticated(req)) {
+		res.send("<div id=\"result\">You are NOT authenticated</div>");
+	}
+	else {
+		res.send("<div id=\"result\">You are authenticated</div>");
+	}
+});
+
+server.get('/profile', function(req, res) {
+		
+	if(!isAuthenticated(req)) {
+		console.log('Session has expired');
+		res.redirect('http://localhost:3000/login.html');
+	}
+
+	var user = getUserFromToken(req);
+
+	if(user !== null) { 
+
+		refreshSession({
+			username: user.username,
+			authenticated: true
+			}, req, res);
+
+		setHeaders(res); 
+
+		var result = "<div id=\"result\">";
+		result += "<br/>username : " + user.username;
+		result += "<br/>email : " + user.email;
+		result += "<br/>firstname : " + user.firstname;
+		result += "<br/>lastname : " + user.lastname;
+		result += "</div>";
+
+		res.send(result);
+	}
+	else {
+		res.send("<div id=\"result\">User not found</div>");
+	}
+});
+
+server.get('/reset', function(req, res) {
+
+	db.clear();
 
 	res.setHeader('Content-Type', 'text/html; charset=utf-8');
 	res.setHeader('X-XSS-Protection', '0'); //ERR_BLOCKED_BY_XSS_AUDITOR
 
-	if(req.cookies["Session-Token"]) {
-		res.send("<div id=\"result\">You are authenticated</div>");
-	}
-	else {
-		res.send("<div id=\"result\">You are NOT authenticated</div>");
-	}
+	req.session = null;
+	res.clearCookie('Session-Token', { path: '/' });
+
+	res.redirect('http://localhost:3000/index.html');
 });
 
+
+
 server.post('/forgotten', function(req, res) {
-	//console.log('you posted:  ' + JSON.stringify(req.body));
-	//console.log('Cookies: ', req.cookies);
 
 	res.setHeader('Content-Type', 'text/html; charset=utf-8');
 	res.setHeader('X-XSS-Protection', '0'); //ERR_BLOCKED_BY_XSS_AUDITOR
@@ -56,12 +96,42 @@ server.post('/search', function(req, res) {
 });
 
 server.post('/login', function(req, res) {
+ 
+	var user = getUser(req.body.username);
 
-	//console.log('you posted:  ' + JSON.stringify(req.body));
+	if(user.username === req.body.username && user.password === req.body.password) { 
 
-	res.cookie('Session-Token',123456789, { maxAge: 900000, httpOnly: true });
-	res.setHeader('Content-Type', 'text/html; charset=utf-8');
-	res.setHeader('X-XSS-Protection', '0'); //ERR_BLOCKED_BY_XSS_AUDITOR
+		refreshSession({
+			username: req.body.username,
+			authenticated: true
+			}, req, res);
+
+		setHeaders(res); 
+		res.send("<div id=\"result\">Welcome: "+ user.username + "</div>");
+	}
+	else {
+		res.send("<div id=\"result\">You are NOT authenticated</div>");
+	}
+
+
+});
+
+server.post('/register', function(req, res) {
+
+	putUser({
+        	username: req.body.username || null,
+        	password: req.body.password || null,
+        	email: req.body.email || null,
+        	firstname: req.body.firstname || null,
+        	lastname: req.body.lastname || null
+        });
+
+	refreshSession({
+		username: req.body.username,
+		authenticated: false
+		}, req, res);
+
+	setHeaders(res); 
 	res.send("<div id=\"result\">Welcome: "+ req.body.username + "</div>");
 });
 
@@ -75,9 +145,70 @@ server.listen(server.get('port'), function() {
 
 
 
+function setHeaders(res) {
+	res.setHeader('Content-Type', 'text/html; charset=utf-8');
+	res.setHeader('X-XSS-Protection', '0'); //ERR_BLOCKED_BY_XSS_AUDITOR
+}
 
 
 /*
+* Session Management
+*/
+function isAuthenticated(req) {
+	if(req.cookies["Session-Token"] !== null) {
+		if(db.get(req.cookies["Session-Token"]) !== null) {
+			console.log('user ' + db.get(req.cookies["Session-Token"]).username + ' has a session: ' + db.get(req.cookies["Session-Token"]).authenticated);
+			return db.get(req.cookies["Session-Token"]).authenticated;
+		}
+	}
+	return false;
+}
+
+
+function refreshSession(session, req, res) {
+
+	//remove previous token from cache if present
+	if(req.cookies["Session-Token"] !== null) {
+		db.del(req.cookies["Session-Token"]);
+	}
+
+	var token = uuid.v4();
+	db.put(token, session, 1800000);		
+	res.cookie('Session-Token', token, { maxAge: 1800000, httpOnly: true });
+
+	console.log("refreshed session: " + token + ' for ' + session.username + ' ' + session.authenticated);
+}
+
+function getSessionToken(req) {
+	return req.cookies["Session-Token"];
+}
+
+function getSession(token) {
+	return db.get(token);
+}
+
+
+function getUserFromToken(req) {
+	return getUser(db.get(req.cookies["Session-Token"]).username);
+}
+
+
+/*
+* User storage
+*/
+function getUser(username) {
+	return JSON.parse(db.get(username));
+}
+
+function putUser(user) {
+	db.put(user.username, JSON.stringify(user));
+}
+
+
+
+/*
+	//console.log('you posted:  ' + JSON.stringify(req.body));
+
 	res.setHeader('Content-Type', 'application/json');
 	res.send(JSON.stringify({
         	username: req.body.username || null,
