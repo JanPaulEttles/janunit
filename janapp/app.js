5b93636f0d3a8ad962e21d608546d8fbe6b3cee1
Jan Paul Ettles <JanPaulEttles>
2017-07-14 15:37:21 +0100

    ssl-ed

--- janapp/app.js
+++ janapp/app.js
@@ -6,24 +6,69 @@ npm install tinycache
 npm install node-uuid
 */
 
+
+/*
+Generate a private key
+openssl genrsa -des3 -out ca.key 2048
+
+Generate the csr (Certificate Signing Request)
+openssl req -new -key ca.key -out ca.csr
+
+Generate self-signed SSL certificate 
+openssl x509 -req -days 365 -in ca.csr -out ca.crt -signkey ca.key
+
+openssl genrsa -des3 -out server.key 2048
+openssl req -new -key server.key -out server.csr
+cp server.key server.key.passphrase
+remove the passphrase
+openssl rsa -in server.key.passphrase -out server.key
+openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
+
+Update the hosts file with the domain
+echo "127.0.0.1 localhost.ssl" | sudo tee -a /etc/hosts
+
+
+With the app running, fetch the cert
+echo QUIT | openssl s_client -connect localhost.ssl:3000 | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > janunitcacert.pem
+Use the cert within curl to test
+curl --cacert janunitcacert.pem https://localhost.ssl:3000/index.html
+
+
+fetch the zap cert
+curl http://host:8080/OTHER/core/other/rootcert/?formMethod=GET > zap.pem
+test with curl specifying zap as the proxy
+curl --proxy localhost.ssl:8080 --cacert zap.pem https://localhost.ssl:3000/index.html
+
+
+*/
 var express = require('express');
 var bodyParser = require('body-parser')
 var cookieParser = require('cookie-parser');
 var tinycache = require('tinycache');
 var uuid = require('node-uuid');
+var https = require('https')
+var fs = require('fs');
+
+var randomXPoweredBy = require('./randomXPoweredBy');
 
 var db = new tinycache();
 
-var server = express();
-server.set('port', 3000);
-server.use(express.static(__dirname + '/'));
+var app = express();
+app.set('port', 3000);
+app.use(express.static(__dirname + '/'));
 
-server.use(cookieParser());
-server.use(bodyParser.json());
-server.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
-server.get('/authenticated', function(req, res) {
+/**
+*
+*	get request to check if the user is authenticated
+*	https://localhost:3000/authenticated
+*
+*/
+app.get('/authenticated', function(req, res) {
 
 	if(!isAuthenticated(req)) {
 		res.send("<div id=\"result\">You are NOT authenticated</div>");
@@ -33,11 +78,16 @@ server.get('/authenticated', function(req, res) {
 	}
 });
 
-server.get('/profile', function(req, res) {
+/**
+*
+*	get request to show the user profile associated with the session token
+*	https://localhost:3000/profile
+*
+*/
+app.get('/profile', function(req, res) {
 		
 	if(!isAuthenticated(req)) {
-		console.log('Session has expired');
-		res.redirect('http://localhost:3000/login.html');
+		res.redirect('https://localhost:3000/login.html');
 	}
 
 	var user = getUserFromToken(req);
@@ -65,7 +115,13 @@ server.get('/profile', function(req, res) {
 	}
 });
 
-server.get('/reset', function(req, res) {
+/**
+*
+*	get request to clear cache, session, cookies and redirect to index.html
+*	https://localhost:3000/reset
+*
+*/
+app.get('/reset', function(req, res) {
 
 	db.clear();
 
@@ -75,19 +131,30 @@ server.get('/reset', function(req, res) {
 	req.session = null;
 	res.clearCookie('Session-Token', { path: '/' });
 
-	res.redirect('http://localhost:3000/index.html');
+	res.redirect('https://localhost:3000/index.html');
 });
 
 
-
-server.post('/forgotten', function(req, res) {
+/**
+*
+*	parse forgotten password form
+*	https://localhost:3000/forgotten.html
+*
+*/
+app.post('/forgotten', function(req, res) {
 
 	res.setHeader('Content-Type', 'text/html; charset=utf-8');
 	res.setHeader('X-XSS-Protection', '0'); //ERR_BLOCKED_BY_XSS_AUDITOR
 	res.send("<div id=\"result\">Your password will be sent to: " + req.body.email + "</div>");
 });
 
-server.post('/search', function(req, res) {
+/**
+*
+*	parse search form
+*	https://localhost:3000/forgotten.html
+*
+*/
+app.post('/search', function(req, res) {
 	//console.log('you posted:  ' + JSON.stringify(req.body));
 
 	res.setHeader('Content-Type', 'text/html; charset=utf-8');
@@ -95,7 +162,13 @@ server.post('/search', function(req, res) {
 	res.send("<div id=\"result\">You searched: " + req.body.search + "</div>");
 });
 
-server.post('/login', function(req, res) {
+/**
+*
+*	parse login form
+*	https://localhost:3000/login.html
+*
+*/
+app.post('/login', function(req, res) {
  
 	var user = getUser(req.body.username);
 
@@ -116,7 +189,13 @@ server.post('/login', function(req, res) {
 
 });
 
-server.post('/register', function(req, res) {
+/**
+*
+*	parse registration form
+*	https://localhost:3000/register.html
+*
+*/
+app.post('/register', function(req, res) {
 
 	putUser({
         	username: req.body.username || null,
@@ -136,16 +215,28 @@ server.post('/register', function(req, res) {
 });
 
 
-server.listen(server.get('port'), function() {
-	console.log('running on... ' + server.get('port'));
-});
 
 
+//app.listen(app.get('port'), function() {
+//	console.log('running on... ' + app.get('port'));
+//});
 
 
+//	passphrase: 'changeit'
+var httpsOptions = {
+	key: fs.readFileSync('./server.key'),
+	cert: fs.readFileSync('./server.crt'),
+    ca: fs.readFileSync('./ca.crt'),
+    requestCert: true,
+    rejectUnauthorized: false
+}
 
+var server = https.createServer(httpsOptions, app).listen(app.get('port'), () => {
+  console.log('server running on port ' + app.get('port'))
+})
 
 function setHeaders(res) {
+	res.setHeader('X-Powered-By', randomXPoweredBy.getRandom());
 	res.setHeader('Content-Type', 'text/html; charset=utf-8');
 	res.setHeader('X-XSS-Protection', '0'); //ERR_BLOCKED_BY_XSS_AUDITOR
 }
@@ -155,10 +246,12 @@ function setHeaders(res) {
 * Session Management
 */
 function isAuthenticated(req) {
-	if(req.cookies["Session-Token"] !== null) {
-		if(db.get(req.cookies["Session-Token"]) !== null) {
-			console.log('user ' + db.get(req.cookies["Session-Token"]).username + ' has a session: ' + db.get(req.cookies["Session-Token"]).authenticated);
-			return db.get(req.cookies["Session-Token"]).authenticated;
+	var token = req.cookies["Session-Token"];
+	if(token !== null) {
+		var session = db.get(token);
+		if(session !== null) {
+			console.log('user ' + session.username + ' has a session: ' + session.authenticated);
+			return session.authenticated;
 		}
 	}
 	return false;
@@ -194,7 +287,7 @@ function getUserFromToken(req) {
 
 
 /*
-* User storage
+* User storage, wrap around the tinycache
 */
 function getUser(username) {
 	return JSON.parse(db.get(username));
@@ -203,15 +296,3 @@ function getUser(username) {
 function putUser(user) {
 	db.put(user.username, JSON.stringify(user));
 }
-
-
-
-/*
-	//console.log('you posted:  ' + JSON.stringify(req.body));
-
-	res.setHeader('Content-Type', 'application/json');
-	res.send(JSON.stringify({
-        	username: req.body.username || null,
-        	email: req.body.email || null
-        }));
-*/
